c144e52e1bdcaaa2fb9c363856aca769f71fa00b
Yuheng Cai <ycai1@flexport.com>
2022-01-29 15:47:50 -0600

    use uuid in item

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import {nanoid} from 'nanoid';
 
 export default class Header extends Component {
 handleEnter = (event) => {
     const {keyCode, target} = event
     if (keyCode != 13) return
-    const toDoObj = {id: 4, name: target.value, done: false}
+    const toDoObj = {id: nanoid(), name: target.value, done: false}
     this.props.addToDo(toDoObj)
 }
   render() {
