1931a437ca3288b5f6a966cc4f9f7be1f81f4eb7
Yuheng Cai <ycai1@flexport.com>
2022-02-06 11:49:35 -0600

    back_to_starter_code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React,{Component} from "react"
-import axios from 'axios'
 
 export default class App extends Component{
-    getStudentData = ()=>{
-        axios.get('http://localhost:3000/students').then(
-            response => {console.log("success!", response.data);},
-            error => {console.log("fail!", error);}
-        )
-    }
-
     render(){
         return (
             <div>
-                <button onClick={this.getStudentData}>Get Student Data</button>
+                hello!
             </div>
         )
     }
