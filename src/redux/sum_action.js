503ec27002e71e0830de1305983e0a3ca2a53e62
Yuheng Cai <ycai1@flexport.com>
2022-02-19 17:26:12 -0600

    use constant to make the action type more manageable

--- src/redux/sum_action.js
+++ src/redux/sum_action.js
@@ -1,2 +1,4 @@
-export const createIncrementAction = (value)=>({type: 'increment', data: value*1})
-export const createDecrementAction = (value)=>({type: 'decrement', data: value*1})
+import { DECREMENT, INCREMENT } from "./constant"
+
+export const createIncrementAction = (value)=>({type: INCREMENT, data: value*1})
+export const createDecrementAction = (value)=>({type: DECREMENT, data: value*1})
