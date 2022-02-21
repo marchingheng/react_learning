53ddd6eec7e35300dbb08513a875210488603b70
Yuheng Cai <ycai1@flexport.com>
2022-02-20 23:09:54 -0600

    simplification: merge container and ui component into one file

--- src/containers/Count/index.jsx
+++ src/containers/Count/index.jsx
@@ -1,7 +1,48 @@
-import CountUI from "../../components/Count"
+import React from 'react';
 import {connect} from 'react-redux'
-import { INCREMENT } from "../../redux/constant"
 import { createDecrementAction, createIncrementAction } from "../../redux/sum_action"
+
+class Count extends React.Component {
+    increament = () => {
+        const {value} = this.selectedNumber
+        this.props.increment(value*1)
+
+    }
+    decreament = () => {
+        const {value} = this.selectedNumber
+        this.props.decrement(value*1)
+    }
+    increamentIfOdd = () => {
+        const {value} = this.selectedNumber
+        if(this.props.count % 2 === 1){
+            this.props.increment(value*1)
+        }
+    }
+    increamentAsync = () => {
+        const {value} = this.selectedNumber
+        setTimeout(() => {
+            this.props.increment(value*1)
+        }, 500)
+    }
+
+    render() {
+        return (
+        <div>
+            <h1>current sum is: {this.props.count}</h1>
+            <select ref={c => this.selectedNumber = c}>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+            </select>&nbsp;
+            <button onClick={this.increament}>+</button>&nbsp;
+            <button onClick={this.decreament}>-</button>&nbsp;
+            <button onClick={this.increamentIfOdd}>if current sum is odd, then increment</button>&nbsp;
+            <button onClick={this.increamentAsync}>increment async</button>&nbsp;
+        </div>
+        );
+    }
+}
 
 // According to the syntax of react-redux, this function is passed as props to CountUI in this way. It is the value that flow: redux store => Count Container => Count UI
 // the argument of this function is the current value in the store
@@ -22,5 +63,5 @@ const mapDispatchToProps = {
 }
 
 // create a Count Container which will interact with redux directly. This Count Container is used in App.js to interact with redux store
-export default connect(mapStateToProps,mapDispatchToProps)(CountUI)
+export default connect(mapStateToProps,mapDispatchToProps)(Count)
 
