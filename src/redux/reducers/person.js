a1f956ca22569da925161c42311debeb99cbab21
Yuheng Cai <ycai1@flexport.com>
2022-02-21 11:53:27 -0600

    Pure Function enforcement of redux

--- src/redux/reducers/person.js
+++ src/redux/reducers/person.js
@@ -5,6 +5,12 @@ export default function personReducer(preState=initState, action){
     const {data, type} = action
     switch (type) {
         case ADD_PERSON:
+            // // You cannot write like this, because this will make personReducer no longer a pure function. Redux enforce the reduces to be pure functions.
+            // // Definition of pure function:
+            // // 1. given the same input will return the same output
+            // // 2. produce no side effect(like setting other variables' value or making requests to other services)
+            // preState.unshift(data)
+            // return preState
             return [data, ...preState];
         default:
             return preState;
