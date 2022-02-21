3ed9c5a621170819fbbb3e72499b07993395acec
Yuheng Cai <ycai1@flexport.com>
2022-02-20 19:38:51 -0600

    some simplification

--- src/containers/Count/index.jsx
+++ src/containers/Count/index.jsx
@@ -5,17 +5,20 @@ import { createDecrementAction, createIncrementAction } from "../../redux/sum_ac
 
 // According to the syntax of react-redux, this function is passed as props to CountUI in this way. It is the value that flow: redux store => Count Container => Count UI
 // the argument of this function is the current value in the store
-function mapStateToProps(state){
-    return {count: state}
-}
+const mapStateToProps = state => ({count: state})
 
-// According to the syntax of react-redux, this function is passed as props to CountUI in this way. It is the callback to set value that flow: redux store => Count Container => Count UI
-// the argument of this function is the dispatch function(action creator) that store can use
-function mapDispatchToProps(dispatch){
-    return {
-        increment: data => dispatch(createIncrementAction(data)),
-        decrement: data => dispatch(createDecrementAction(data))
-    }
+// // According to the syntax of react-redux, this function is passed as props to CountUI in this way. It is the callback to set value that flow: redux store => Count Container => Count UI
+// // the argument of this function is the dispatch function(action creator) that store can use
+// function mapDispatchToProps(dispatch){
+//     return {
+//         increment: data => dispatch(createIncrementAction(data)),
+//         decrement: data => dispatch(createDecrementAction(data))
+//     }
+// }
+// // You can also simplified it as an object like below, this kind of simplification is defined within react-redux
+const mapDispatchToProps = {
+    increment: createIncrementAction,
+    decrement: createDecrementAction
 }
 
 // create a Count Container which will interact with redux directly. This Count Container is used in App.js to interact with redux store
