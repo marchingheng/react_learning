677b0f6d6bfff6fc055485c0f1d5a423f908839f
Yuheng Cai <ycai1@flexport.com>
2022-02-21 12:10:32 -0600

    use redux devtool

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,14 @@
 import {createStore, combineReducers} from 'redux'
 import countReducer from './reducers/count'
 import personReducer from './reducers/person'
+// Steps to use redux devtool:
+// 1. install the chrome extension: https://chrome.google.com/webstore/detail/redux-devtools/
+// 2. npm install 'redux-devtools-extension'
+// 3. config the devtool in store
+import {composeWithDevTools} from 'redux-devtools-extension'
 const allReducers = combineReducers({
     sum: countReducer, 
     people: personReducer
 })
 
-export default createStore(allReducers)
+export default createStore(allReducers, composeWithDevTools())
