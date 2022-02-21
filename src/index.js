6d8d45d30e469bcb6682d0d85de829e8f930cf9b
Yuheng Cai <ycai1@flexport.com>
2022-02-20 19:50:09 -0600

    Provider: an easier way to pass value from redux store to Container Component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import reactDom from 'react-dom'
 import App from './App'
 import store from './redux/store'
+import {Provider} from 'react-redux'
 
-reactDom.render(<App/>, document.getElementById('root'))
-// You can either the store.subscribe in index.js or in Count component.
-// If you put it here, then when there is any change to the state inside of store, we will re-render the entire App.
-// Don't worry, the react DOM diff algorithm will compared all the components and only re-render the changed components. Therefore the computation is not too heavy.
-store.subscribe(
-    ()=>{reactDom.render(<App/>, document.getElementById('root'))}
+reactDom.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
 )
